cc5deb83adf30f947e58c17310051c45e005c2b0
GewoonRene <rhuibertsjr@gmail.com>
2019-05-12 18:53:38 +0200

    Updated: Readme

--- config/common.config.js
+++ config/common.config.js
@@ -11,7 +11,7 @@ module.exports = {
         publicPath: "/"
     },
     devServer: {
-        port: 3042,
+        port: 8080,
         historyApiFallback: true,
         overlay: true,
         open: true,
