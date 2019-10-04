426e6fdeb042fc9b1fcd011d7f2cde07c3b18389
GewoonRene <rhuibertsjr@gmail.com>
2019-10-04 11:31:59 +0200

    Fixed: Wrong import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './styles/index.scss';
+import './assets/scss/index.scss';
 
 "use strict"
 const Index: React.FC = () => {
