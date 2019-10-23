11c478891e908564b0db2cce5489b9b309f57dde
GewoonRene <rhuibertsjr@gmail.com>
2019-10-23 20:41:43 +0200

    Fixed: ES8 Features (#14)
    Async / wait functions can now be used through the project

--- config/common.config.js
+++ config/common.config.js
@@ -32,7 +32,8 @@ module.exports = {
                             {
                                 'plugins': [
                                     '@babel/plugin-proposal-class-properties',
-                                    "@babel/proposal-object-rest-spread"
+                                    "@babel/proposal-object-rest-spread",
+                                    "@babel/plugin-transform-runtime"
                                 ]
                             }
                         ]
